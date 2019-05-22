3bd41348d2eacc0d62a22861e3614c4281d2250a
ruyu.li <ruyu.li>
2019-05-22 09:33:14 +0800

    init

--- micro-biz/src/main/java/cn/micro/biz/commons/mybatis/extension/IMicroService.java
+++ micro-biz/src/main/java/cn/micro/biz/commons/mybatis/extension/IMicroService.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.io.Serializable;
 import java.util.Collection;
 import java.util.List;
 
@@ -25,7 +26,7 @@ public interface IMicroService<T> extends IService<T> {
      * @param eqValue  eg: "Tom"
      * @return {@link T }
      */
-    default T getOne(SFunction<T, ?> eqColumn, Object eqValue) {
+    default T getOne(SFunction<T, String> eqColumn, Serializable eqValue) {
         return getOne(eqColumn, eqValue, null, null);
     }
 
@@ -38,8 +39,9 @@ public interface IMicroService<T> extends IService<T> {
      * @param eqValue2  eg: "STUDENT"
      * @return {@link T }
      */
-    default T getOne(SFunction<T, ?> eqColumn1, Object eqValue1, SFunction<T, ?> eqColumn2, Object eqValue2) {
-        return getOne(buildEqQuery(eqColumn1, eqValue1, eqColumn2, eqValue2));
+    default T getOne(SFunction<T, String> eqColumn1, Serializable eqValue1,
+                     SFunction<T, String> eqColumn2, Serializable eqValue2) {
+        return this.getOne(buildEqQuery(eqColumn1, eqValue1, eqColumn2, eqValue2));
     }
 
     // ====== lambda equals(eq) to list object
@@ -51,7 +53,7 @@ public interface IMicroService<T> extends IService<T> {
      * @param eqValue  eg: "Tom"
      * @return {@link T }
      */
-    default List<T> list(SFunction<T, ?> eqColumn, Object eqValue) {
+    default List<T> list(SFunction<T, String> eqColumn, Serializable eqValue) {
         return list(eqColumn, eqValue, null, null);
     }
 
@@ -64,8 +66,78 @@ public interface IMicroService<T> extends IService<T> {
      * @param eqValue2  eg: "STUDENT"
      * @return {@link T }
      */
-    default List<T> list(SFunction<T, ?> eqColumn1, Object eqValue1, SFunction<T, ?> eqColumn2, Object eqValue2) {
-        return list(buildEqQuery(eqColumn1, eqValue1, eqColumn2, eqValue2));
+    default List<T> list(SFunction<T, String> eqColumn1, Serializable eqValue1,
+                         SFunction<T, String> eqColumn2, Serializable eqValue2) {
+        return this.list(buildEqQuery(eqColumn1, eqValue1, eqColumn2, eqValue2));
+    }
+
+    // ====== lambda equals(eq) to one object
+
+    /**
+     * The remove by 1 equals(eq) bean field
+     *
+     * @param eqColumn eg: User::getName
+     * @param eqValue  eg: "Tom"
+     * @return {@link T }
+     */
+    default boolean remove(SFunction<T, String> eqColumn, Serializable eqValue) {
+        return remove(eqColumn, eqValue, null, null);
+    }
+
+    /**
+     * The remove by 2 equals(eq) bean field
+     *
+     * @param eqColumn1 eg: User::getName
+     * @param eqValue1  eg: "Tom"
+     * @param eqColumn2 eg: User::getCategory
+     * @param eqValue2  eg: "STUDENT"
+     * @return {@link T }
+     */
+    default boolean remove(
+            SFunction<T, String> eqColumn1, Serializable eqValue1,
+            SFunction<T, ?> eqColumn2, Serializable eqValue2) {
+        LambdaQueryWrapper<T> wrapper = new LambdaQueryWrapper<T>().eq(eqColumn1, eqValue1);
+        if (eqColumn2 != null && eqValue2 != null) {
+            wrapper.eq(eqColumn2, eqValue2);
+        }
+
+        return this.remove(wrapper);
+    }
+
+    // ====== lambda equals(eq) to one object
+
+    /**
+     * The update by 1 equals(eq) bean field
+     *
+     * @param entity   update entity
+     * @param eqColumn eg: User::getName
+     * @param eqValue  eg: "Tom"
+     * @return {@link T }
+     */
+    default boolean update(T entity, SFunction<T, String> eqColumn, Serializable eqValue) {
+        return update(entity, eqColumn, eqValue, null, null);
+    }
+
+    /**
+     * The update by 2 equals(eq) bean field
+     *
+     * @param entity    update entity
+     * @param eqColumn1 eg: User::getName
+     * @param eqValue1  eg: "Tom"
+     * @param eqColumn2 eg: User::getCategory
+     * @param eqValue2  eg: "STUDENT"
+     * @return {@link T }
+     */
+    default boolean update(
+            T entity,
+            SFunction<T, String> eqColumn1, Serializable eqValue1,
+            SFunction<T, ?> eqColumn2, Serializable eqValue2) {
+        LambdaQueryWrapper<T> wrapper = new LambdaQueryWrapper<T>().eq(eqColumn1, eqValue1);
+        if (eqColumn2 != null && eqValue2 != null) {
+            wrapper.eq(eqColumn2, eqValue2);
+        }
+
+        return this.update(entity, wrapper);
     }
 
     // ====== lambda in to list object
@@ -77,8 +149,8 @@ public interface IMicroService<T> extends IService<T> {
      * @param inValues eg: "Tom", "Jack"
      * @return {@link List<T> }
      */
-    default List<T> listIn(SFunction<T, ?> inColumn, Object... inValues) {
-        return list(new LambdaQueryWrapper<T>().in(inColumn, inValues));
+    default List<T> listIn(SFunction<T, String> inColumn, Object... inValues) {
+        return this.list(new LambdaQueryWrapper<T>().in(inColumn, inValues));
     }
 
     /**
@@ -88,8 +160,8 @@ public interface IMicroService<T> extends IService<T> {
      * @param inValues eg: Arrays.asList("Tom", "Jack")
      * @return {@link List<T> }
      */
-    default List<T> listIn(SFunction<T, ?> inColumn, Collection<?> inValues) {
-        return list(new LambdaQueryWrapper<T>().in(inColumn, inValues));
+    default List<T> listIn(SFunction<T, String> inColumn, Collection<Serializable> inValues) {
+        return this.list(new LambdaQueryWrapper<T>().in(inColumn, inValues));
     }
 
     // ====== lambda equals(eq) and in to list object
@@ -104,8 +176,8 @@ public interface IMicroService<T> extends IService<T> {
      * @return {@link List<T> }
      */
     default List<T> listEqAndIn(
-            SFunction<T, ?> eqColumn, Object eqValue,
-            SFunction<T, ?> inColumn, Object... inValues) {
+            SFunction<T, String> eqColumn, Serializable eqValue,
+            SFunction<T, String> inColumn, Object... inValues) {
         return listEqAndIn(eqColumn, eqValue, null, null, inColumn, inValues);
     }
 
@@ -121,10 +193,10 @@ public interface IMicroService<T> extends IService<T> {
      * @return {@link List<T> }
      */
     default List<T> listEqAndIn(
-            SFunction<T, ?> eqColumn1, Object eqValue1,
-            SFunction<T, ?> eqColumn2, Object eqValue2,
-            SFunction<T, ?> inColumn, Object... inValues) {
-        return list(buildEqQuery(eqColumn1, eqValue1, eqColumn2, eqValue2).in(inColumn, inValues));
+            SFunction<T, String> eqColumn1, Serializable eqValue1,
+            SFunction<T, String> eqColumn2, Serializable eqValue2,
+            SFunction<T, String> inColumn, Object... inValues) {
+        return this.list(buildEqQuery(eqColumn1, eqValue1, eqColumn2, eqValue2).in(inColumn, inValues));
     }
 
     /**
@@ -137,8 +209,8 @@ public interface IMicroService<T> extends IService<T> {
      * @return {@link List<T> }
      */
     default List<T> listEqAndIn(
-            SFunction<T, ?> eqColumn, Object eqValue,
-            SFunction<T, ?> inColumn, Collection<?> inValues) {
+            SFunction<T, String> eqColumn, Serializable eqValue,
+            SFunction<T, String> inColumn, Collection<Serializable> inValues) {
         return listEqAndIn(eqColumn, eqValue, null, null, inColumn, inValues);
     }
 
@@ -154,10 +226,10 @@ public interface IMicroService<T> extends IService<T> {
      * @return {@link List<T> }
      */
     default List<T> listEqAndIn(
-            SFunction<T, ?> eqColumn1, Object eqValue1,
-            SFunction<T, ?> eqColumn2, Object eqValue2,
-            SFunction<T, ?> inColumn, Collection<?> inValues) {
-        return list(buildEqQuery(eqColumn1, eqValue1, eqColumn2, eqValue2).in(inColumn, inValues));
+            SFunction<T, String> eqColumn1, Serializable eqValue1,
+            SFunction<T, String> eqColumn2, Serializable eqValue2,
+            SFunction<T, String> inColumn, Collection<Serializable> inValues) {
+        return this.list(buildEqQuery(eqColumn1, eqValue1, eqColumn2, eqValue2).in(inColumn, inValues));
     }
 
     /**
@@ -169,7 +241,9 @@ public interface IMicroService<T> extends IService<T> {
      * @param value2  eg: "STUDENT"
      * @return {@link LambdaQueryWrapper<T>}
      */
-    default LambdaQueryWrapper<T> buildEqQuery(SFunction<T, ?> column1, Object value1, SFunction<T, ?> column2, Object value2) {
+    default LambdaQueryWrapper<T> buildEqQuery(
+            SFunction<T, String> column1, Serializable value1,
+            SFunction<T, String> column2, Serializable value2) {
         LambdaQueryWrapper<T> wrapper = new LambdaQueryWrapper<T>().eq(column1, value1);
         if (column2 != null && value2 != null) {
             wrapper.eq(column2, value2);
