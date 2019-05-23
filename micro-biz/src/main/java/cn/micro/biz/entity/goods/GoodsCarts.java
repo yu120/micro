f4620e6c5c8b5f5dd11acf549e8873c575d45574
lry <595208882@qq.com>
2019-05-24 00:28:20 +0800

    init

---
+++ micro-biz/src/main/java/cn/micro/biz/entity/goods/GoodsCarts.java
@@ -0,0 +1,46 @@
+package cn.micro.biz.entity.goods;
+
+import cn.micro.biz.commons.mybatis.entity.MicroEntity;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.*;
+
+/**
+ * Goods Carts Entity
+ *
+ * @author lry
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@TableName("goods_carts")
+public class GoodsCarts extends MicroEntity<GoodsCarts> {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Member id
+     * <p>
+     * {@link cn.micro.biz.entity.member.Member#id}
+     **/
+    private Long memberId;
+    /**
+     * Goods id
+     * <p>
+     * {@link Goods#id}
+     **/
+    private Long goodsId;
+    /**
+     * Goods specification id
+     * <p>
+     * {@link GoodsSpecification#id}
+     **/
+    private Long goodsSpecificationId;
+
+    /**
+     * Goods quantity
+     */
+    private Integer quantity;
+
+}
