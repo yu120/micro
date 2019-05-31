0a9c36855061652820e597a685198468cda632e9
lry <595208882@qq.com>
2019-06-01 07:58:13 +0800

    init

--- micro-biz/src/main/java/cn/micro/biz/web/PublicController.java
+++ micro-biz/src/main/java/cn/micro/biz/web/PublicController.java
@@ -1,6 +1,9 @@
 package cn.micro.biz.web;
 
+import cn.micro.biz.commons.auth.NonAuth;
 import cn.micro.biz.commons.auth.PreAuth;
+import cn.micro.biz.model.vo.OssTokenVO;
+import cn.micro.biz.pubsrv.oss.QiNiuOssService;
 import cn.micro.biz.pubsrv.sms.AliYunSmsService;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,20 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 /**
- * SMS Controller
+ * Public Service Controller
  *
  * @author lry
  */
 @PreAuth
 @Validated
 @RestController
-@RequestMapping("sms")
-public class SmsController {
+public class PublicController {
 
+    @Resource
+    private QiNiuOssService qiNiuOssService;
     @Resource
     private AliYunSmsService aliYunSmsService;
 
-    @RequestMapping(value = "send", method = RequestMethod.GET)
+    @NonAuth
+    @RequestMapping(value = "oss/token", method = RequestMethod.GET)
+    public OssTokenVO token() {
+        return qiNiuOssService.uploadToken();
+    }
+
+    @RequestMapping(value = "sms/send", method = RequestMethod.GET)
     public Boolean send() throws Exception {
         return aliYunSmsService.send("SMS_151232xxx", "15888888888", "李景枫", "电脑", "10001");
     }
