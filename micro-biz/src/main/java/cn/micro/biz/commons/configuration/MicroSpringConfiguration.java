1e496f0655fca7241571b748bbd082eafaa8aad6
lry <595208882@qq.com>
2019-06-04 01:19:53 +0800

    init

--- micro-biz/src/main/java/cn/micro/biz/commons/configuration/MicroSpringConfiguration.java
+++ micro-biz/src/main/java/cn/micro/biz/commons/configuration/MicroSpringConfiguration.java
@@ -1,7 +1,13 @@
 package cn.micro.biz.commons.configuration;
 
 import cn.micro.biz.commons.auth.GlobalAuthHandlerInterceptor;
+import cn.micro.biz.commons.mybatis.extension.enums.IEnum;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
@@ -13,10 +19,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Nullable;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -38,12 +46,31 @@ public class MicroSpringConfiguration implements ApplicationContextAware, Initia
 
     private final MicroProperties microProperties;
     private final GlobalAuthHandlerInterceptor globalAuthHandlerInterceptor;
+    private final MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter;
 
     @Override
     public void setApplicationContext(@Nullable ApplicationContext applicationContext) throws BeansException {
         if (applicationContext != null) {
             MicroSpringConfiguration.APPLICATION_CONTEXT = applicationContext;
             MicroSpringConfiguration.BASE_PACKAGES = AutoConfigurationPackages.get(applicationContext);
+            // set enum object mapper
+            mappingJackson2HttpMessageConverter.setObjectMapper(new MicroObjectMapper());
+        }
+    }
+
+    public static class MicroObjectMapper extends ObjectMapper {
+        public MicroObjectMapper() {
+            super();
+            SimpleModule module = new SimpleModule();
+            module.addSerializer(IEnum.class, new JsonSerializer<IEnum>() {
+                @Override
+                public void serialize(IEnum value, JsonGenerator gen,
+                                      SerializerProvider serializers) throws IOException {
+                    gen.writeObject(value.getValue());
+                }
+            });
+
+            super.registerModule(module);
         }
     }
 
