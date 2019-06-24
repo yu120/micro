89213e821bffd83c45d926d8548aaf7adcf59ffb
lry <lry>
2019-06-24 10:37:35 +0800

    init

--- micro-biz/src/main/java/cn/micro/biz/service/member/impl/MemberServiceImpl.java
+++ micro-biz/src/main/java/cn/micro/biz/service/member/impl/MemberServiceImpl.java
@@ -2,12 +2,14 @@ package cn.micro.biz.service.member.impl;
 
 import cn.micro.biz.commons.auth.MicroAuthContext;
 import cn.micro.biz.commons.mybatis.extension.MicroServiceImpl;
-import cn.micro.biz.convert.MemberConvert;
 import cn.micro.biz.entity.member.Member;
 import cn.micro.biz.mapper.member.IMemberMapper;
 import cn.micro.biz.model.edit.EditMemberInfo;
 import cn.micro.biz.service.member.IMemberService;
+import com.github.dozermapper.core.Mapper;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * Member Service Implements
@@ -17,6 +19,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class MemberServiceImpl extends MicroServiceImpl<IMemberMapper, Member> implements IMemberService {
 
+    @Resource
+    private Mapper mapper;
+
     @Override
     public Member info() {
         Long memberId = MicroAuthContext.getMemberId();
@@ -25,7 +30,7 @@ public class MemberServiceImpl extends MicroServiceImpl<IMemberMapper, Member> i
 
     @Override
     public Boolean edit(EditMemberInfo memberInfo) {
-        Member member = MemberConvert.INSTANCE.copy(memberInfo);
+        Member member = mapper.map(memberInfo, Member.class);
         member.setId(MicroAuthContext.getMemberId());
         return super.updateById(member);
     }
