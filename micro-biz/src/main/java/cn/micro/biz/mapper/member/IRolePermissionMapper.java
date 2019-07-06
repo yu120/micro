f1a2f347bbeffd647c6bb33a82c31bb137f37ed0
lry <595208882@qq.com>
2019-07-06 09:31:17 +0800

    init

--- micro-biz/src/main/java/cn/micro/biz/mapper/member/IRolePermissionMapper.java
+++ micro-biz/src/main/java/cn/micro/biz/mapper/member/IRolePermissionMapper.java
@@ -2,6 +2,9 @@ package cn.micro.biz.mapper.member;
 
 import cn.micro.biz.commons.mybatis.extension.IMicroMapper;
 import cn.micro.biz.entity.member.RolePermission;
+import cn.micro.biz.model.view.RoleCodePermission;
+
+import java.util.List;
 
 /**
  * Role Permission Mapper
@@ -9,5 +12,20 @@ import cn.micro.biz.entity.member.RolePermission;
  * @author lry
  */
 public interface IRolePermissionMapper extends IMicroMapper<RolePermission> {
+
+    /**
+     * The select roleCode permission list
+     *
+     * @return {@link List<RoleCodePermission>}
+     */
+    List<RoleCodePermission> selectRoleCodePermissions();
+
+    /**
+     * The select permission list by roleCode
+     *
+     * @param roleCode {@link cn.micro.biz.entity.member.Role#code}
+     * @return {@link List<RoleCodePermission>}
+     */
+    List<RoleCodePermission> selectPermissionsByRoleCode(String roleCode);
 
 }
---
+++ micro-biz/src/main/java/cn/micro/biz/model/view/RoleCodePermission.java
@@ -0,0 +1,19 @@
+package cn.micro.biz.model.view;
+
+import cn.micro.biz.entity.member.Permission;
+
+/**
+ * Role Code Permission
+ *
+ * @author lry
+ */
+public class RoleCodePermission extends Permission {
+
+    /**
+     * Member id
+     * <p>
+     * {@link cn.micro.biz.entity.member.Role#code}
+     */
+    private String roleCode;
+
+}
