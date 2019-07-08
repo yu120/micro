e14a1bfe0564c2c12b69a2a3cbb17f72f4f07baf
lry <595208882@qq.com>
2019-07-09 00:52:05 +0800

    init

--- micro-dubbo/src/main/java/cn/micro/biz/dubbo/Application.java
+++ micro-dubbo/src/main/java/cn/micro/biz/dubbo/Application.java
@@ -1,22 +1,20 @@
 package cn.micro.biz.dubbo;
 
-import cn.micro.biz.dubbo.provider.DemoService;
-import cn.micro.biz.dubbo.provider.impl.DemoServiceImpl;
-import org.apache.dubbo.config.ApplicationConfig;
-import org.apache.dubbo.config.RegistryConfig;
-import org.apache.dubbo.config.ServiceConfig;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.PropertySource;
 
 import java.io.IOException;
 
+@EnableDubbo(scanBasePackages = "cn.micro.biz.dubbo.provider.impl")
+@PropertySource("classpath:dubbo-provider.properties")
 public class Application {
 
     public static void main(String[] args) throws IOException {
-        ServiceConfig<DemoService> serviceConfig = new ServiceConfig<>();
-        serviceConfig.setApplication(new ApplicationConfig("micro-dubbo-provider"));
-        serviceConfig.setRegistry(new RegistryConfig("zookeeper://127.0.0.1:2181"));
-        serviceConfig.setInterface(DemoService.class);
-        serviceConfig.setRef(new DemoServiceImpl());
-        serviceConfig.export();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(Application.class);
+        context.refresh();
+        System.out.println("DemoService provider is starting...");
         System.in.read();
     }
 }
--- micro-dubbo/src/main/java/cn/micro/biz/dubbo/provider/impl/DemoServiceImpl.java
+++ micro-dubbo/src/main/java/cn/micro/biz/dubbo/provider/impl/DemoServiceImpl.java
@@ -1,8 +1,11 @@
 package cn.micro.biz.dubbo.provider.impl;
 
 import cn.micro.biz.dubbo.provider.DemoService;
+import org.apache.dubbo.config.annotation.Service;
 
+@Service
 public class DemoServiceImpl implements DemoService {
+
     @Override
     public String sayHello(String name) {
         System.out.println("===" + name);
