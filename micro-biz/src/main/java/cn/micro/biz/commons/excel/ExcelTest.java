2e29db5dd9004ffa08443e4a895069557b306eac
lry <lry>
2019-09-09 17:02:49 +0800

    Excel导入导出下载工具

--- micro-biz/src/main/java/cn/micro/biz/commons/excel/ExcelTest.java
+++ micro-biz/src/main/java/cn/micro/biz/commons/excel/ExcelTest.java
@@ -11,7 +11,7 @@ public class ExcelTest {
 
     public static void main(String[] args) throws IOException {
         FileInputStream fileInputStream = new FileInputStream(new File("/Users/lry/Downloads/课表-上传模板-星期竖版2.xlsx"));
-        List<List<List<ExcelCell>>> data = ExcelImportUtils.parseSheet2007(false, "\n", "/", fileInputStream);
+        List<List<List<ExcelCell>>> data = ExcelImportUtils.readSheet2007(false, "\n", "/", fileInputStream);
         System.out.println(JSON.toJSONString(data));
     }
 
