bff73594b8d3cb218ca9021b5bb0ff405aa3ea55
lry <595208882@qq.com>
2019-10-27 14:40:18 +0800

    init

--- micro-biz/src/main/java/cn/micro/biz/pubsrv/event/EventCollect.java
+++ micro-biz/src/main/java/cn/micro/biz/pubsrv/event/EventCollect.java
@@ -13,13 +13,31 @@ import java.util.List;
 @Data
 public class EventCollect implements Serializable {
 
+    /**
+     * The event collect title
+     */
     private String title;
+    /**
+     * The event collect describe
+     */
     private String desc;
 
+    /**
+     * The event collect class name
+     */
     private String className;
+    /**
+     * The event collect method name
+     */
     private String methodName;
 
+    /**
+     * The event collect arg list
+     */
     private List<Object> args;
+    /**
+     * The event collect response result
+     */
     private Object result;
 
 }
